85b0ba119d196b4105241cbacb7aedc05002b877
Marcos <aurelio.marcos@ufv.br>
2021-06-14 11:01:57 -0300

    Setup ViewPager in Activity

--- SwipeViews/app/src/main/java/com/example/swipeviews/MainActivity.java
+++ SwipeViews/app/src/main/java/com/example/swipeviews/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.swipeviews;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.ViewPropertyAnimator;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //actionBar
+    private ActionBar actionBar;
+
+    //UI Views
+    private ViewPager viewPager;
+
+    private ArrayList<MyModel> modelArrayList;
+    private MyAdapter myAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init actionbar
+        actionBar = getSupportActionBar();
+
+        //init UI views
+        viewPager = findViewById(R.id.viewPager);
+        modelArrayList = MyModel.loadCards();
+        //setup adapter
+        myAdapter = new MyAdapter(this,modelArrayList);
+
+        //set adapter to view pager
+        viewPager.setAdapter(myAdapter);
+
+        //set default padding from left/right
+        viewPager.setPadding(100,0,100,0);
+        viewPager.addOnPageChangeListener(onPageChangeListener());
+    }
+    private ViewPager.OnPageChangeListener onPageChangeListener(){
+        return new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                //Will just change the title of actionbar
+                String title = modelArrayList.get(position).getTitle();
+                actionBar.setTitle(title);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        };
     }
 }
--- SwipeViews/app/src/main/java/com/example/swipeviews/MyModel.java
+++ SwipeViews/app/src/main/java/com/example/swipeviews/MyModel.java
@@ -1,5 +1,8 @@
 package com.example.swipeviews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyModel {
     String title,description,date;
     int image;
@@ -11,6 +14,15 @@ public class MyModel {
         this.image = image;
     }
 
+    public static ArrayList<MyModel> loadCards(){
+        ArrayList<MyModel> myModelList = new ArrayList<>();
+        myModelList.add(new MyModel("Title 1","Description 1","01/01/01",R.drawable.brochure));
+        myModelList.add(new MyModel("Title 2","Description 2","02/02/02",R.drawable.namecard));
+        myModelList.add(new MyModel("Title 3","Description 3","03/03/03",R.drawable.poster));
+        myModelList.add(new MyModel("Title 4","Description 4","04/04/04",R.drawable.sticker));
+        myModelList.add(new MyModel("Title 5","Description 5","05/05/05",R.drawable.ic_launcher_foreground));
+        return myModelList;
+    }
     public String getTitle() {
         return title;
     }
